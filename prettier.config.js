10e68ea4b908ba368af712a273a3e8d243e0a279
Darren Wong <wongdarren1997@gmail.com>
2022-08-08 20:05:03 -0500

    NextJs + Storybook + ChakraUI

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+module.exports = options;
