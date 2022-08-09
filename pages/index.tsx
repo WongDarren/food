c738b7f235185c35f2301f77b57f622e8d631286
Darren Wong <wongdarren1997@gmail.com>
2022-08-08 20:07:43 -0500

    NextJs + Storybook + ChakraUI (#1)

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>Main</h1>
+      </main>
+
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
